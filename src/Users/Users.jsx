cca6de3239a17a1780a4cf640b913d370ff51edd
Raja Tahseen <rajatahseen86@outlook.com>
2025-05-30 02:05:29 +0500

    Fetch and render user data using useEffect() hook and add crud buttons

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,16 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import axios from "axios";
 
 function Users() {
-  const [products, setProducts] = useState([
-    {
-      code: "1",
-      name: "Product 1",
-      category: "Test Category",
-      quantity: "5",
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [showViewMode, setShowViewMode] = useState(false);
+  const [showAddMode, setShowAddMode] = useState(false);
+  const [showEditMode, setShowEditMode] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/users");
+      if (response) {
+        console.log(response.data);
+      }
+
+      setUsers(response.data);
+    } catch (error) {
+      console.log(e);
+    }
+  };
+
+  const actionsTemplate = (rowDate) => {
+    return (
+      <>
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            setSelectedUserId(rowDate.id);
+            setShowViewMode(true);
+          }}
+        >
+          <i className="pi pi-eye"></i>
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setSelectedUserId(rowDate.id);
+            setShowEditMode(true);
+          }}
+        >
+          <i className="pi pi-file-edit"></i>
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => deleteUserConfirm(rowDate.id)}
+        >
+          <i className="pi pi-trash"></i>
+        </button>
+      </>
+    );
+  };
+
   return (
     <div className="users-page">
       <div className="container">
@@ -18,11 +65,13 @@ function Users() {
         <h3>We will use React, Primereact, Json-Server and Axios</h3>
 
         <div className="users-list">
-          <DataTable value={products} tableStyle={{ minWidth: "50rem" }}>
-            <Column field="code" header="Code"></Column>
+          <DataTable value={users} tableStyle={{ minWidth: "50rem" }}>
             <Column field="name" header="Name"></Column>
-            <Column field="category" header="Category"></Column>
-            <Column field="quantity" header="Quantity"></Column>
+            <Column field="username" header="User Name"></Column>
+            <Column field="email" header="Email"></Column>
+            <Column field="phone" header="Phone"></Column>
+            <Column field="website" header="Website"></Column>
+            <Column header="Actions" body={actionsTemplate}></Column>
           </DataTable>
         </div>
       </div>
